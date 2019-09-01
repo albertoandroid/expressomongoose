5e5a5cbc12451fd76f86a0d4872984b627273cab
Alberto <info@androiddesdecero.com>
2019-09-01 18:48:33 +0200

    login

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt')
+const mongosee = require('mongoose')
+const express = require('express')
+const User = require('../models/user')
+const router = express.Router()
+const { check, validationResult } = require('express-validator');
+
+router.post('/', [
+    check('email').isLength({min: 3}),
+    check('password').isLength({min: 3}),
+],async(req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    let user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).send('Usuario o contraseña incorrectos')
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if(!validPassword) return res.status(400).send('Usuario o contraseña incorrectos')
+
+    res.send('Usuario y contreña correcta')
+})
